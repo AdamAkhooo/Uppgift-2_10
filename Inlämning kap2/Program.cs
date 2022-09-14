32e7a89ec20e0b39715ba51dda3b92105004fbaf
Admin <Admin@TEB123007>
2022-09-14 11:40:15 +0200

    Add project files.

---
+++ Inlämning kap2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Uppgift_Kap_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vilken är din favorit bok?");
+            string bok = Console.ReadLine();
+            Console.WriteLine("Så din favorit bok är alltså " + bok + ".");
+
+
+        }
+    }
+}
